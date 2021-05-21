af243857faeb095f0dd6f50e588e9a060eaff057
Stefano Brivio <sbrivio@redhat.com>
2021-05-21 11:14:49 +0200

    qrap: Silence gcc -O3 warning about strncpy() buffer length
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

--- qrap.c
+++ qrap.c
@@ -109,7 +109,7 @@ int main(int argc, char **argv)
 	if (argc >= 3) {
 		fd = strtol(argv[1], NULL, 0);
 		if (fd >= 3 && fd < INT_MAX && !errno) {
-			char env_path[ARG_MAX], *p, command[ARG_MAX];
+			char env_path[ARG_MAX + 1], *p, command[ARG_MAX];
 
 			strncpy(env_path, getenv("PATH"), ARG_MAX);
 			p = strtok(env_path, ":");
