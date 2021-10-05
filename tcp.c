cf9976beac01301a07536e406fee10beac9c4fd2
Stefano Brivio <sbrivio@redhat.com>
2021-10-05 19:31:04 +0200

    tcp: Increase TCP_TAP_FRAMES once more
    With an increased sending buffer size for the AF_UNIX socket, we
    can get slightly lower overhead.
    
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

--- tcp.c
+++ tcp.c
@@ -339,7 +339,7 @@
 #define MAX_TAP_CONNS			(128 * 1024)
 #define MAX_SPLICE_CONNS		(128 * 1024)
 
-#define TCP_TAP_FRAMES			32
+#define TCP_TAP_FRAMES			64
 
 #define MAX_PIPE_SIZE			(2 * 1024 * 1024)
 
