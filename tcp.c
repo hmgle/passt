hmgle/passt

commit 33482d5bf29312464b208beb01a5302257e82fe6

passt: Add PASTA mode, major rework
PASTA (Pack A Subtle Tap Abstraction) provides quasi-native host
connectivity to an otherwise disconnected, unprivileged network
and user namespace, similarly to slirp4netns. Given that the
implementation is largely overlapping with PASST, no separate binary
is built: 'pasta' (and 'passt4netns' for clarity) both link to
'passt', and the mode of operation is selected depending on how the
binary is invoked. Usage example:

	$ unshare -rUn
	# echo $$
	1871759

	$ ./pasta 1871759	# From another terminal

	# udhcpc -i pasta0 2>/dev/null
	# ping -c1 pasta.pizza
	PING pasta.pizza (64.190.62.111) 56(84) bytes of data.
	64 bytes from 64.190.62.111 (64.190.62.111): icmp_seq=1 ttl=255 time=34.6 ms

	--- pasta.pizza ping statistics ---
	1 packets transmitted, 1 received, 0% packet loss, time 0ms
	rtt min/avg/max/mdev = 34.575/34.575/34.575/0.000 ms
	# ping -c1 spaghetti.pizza
	PING spaghetti.pizza(2606:4700:3034::6815:147a (2606:4700:3034::6815:147a)) 56 data bytes
	64 bytes from 2606:4700:3034::6815:147a (2606:4700:3034::6815:147a): icmp_seq=1 ttl=255 time=29.0 ms

	--- spaghetti.pizza ping statistics ---
	1 packets transmitted, 1 received, 0% packet loss, time 0ms
	rtt min/avg/max/mdev = 28.967/28.967/28.967/0.000 ms

This entails a major rework, especially with regard to the storage of
tracked connections and to the semantics of epoll(7) references.

Indexing TCP and UDP bindings merely by socket proved to be
inflexible and unsuitable to handle different connection flows: pasta
also provides Layer-2 to Layer-2 socket mapping between init and a
separate namespace for local connections, using a pair of splice()
system calls for TCP, and a recvmmsg()/sendmmsg() pair for UDP local
bindings. For instance, building on the previous example:

	# ip link set dev lo up
	# iperf3 -s

	$ iperf3 -c ::1 -Z -w 32M -l 1024k -P2 | tail -n4
	[SUM]   0.00-10.00  sec  52.3 GBytes  44.9 Gbits/sec  283             sender

tcp.h
@@ -3,9 +3,12 @@
 
 #define TCP_TIMER_INTERVAL		20 /* ms */
 
+#define TCP_MAX_CONNS			(128 * 1024)
+#define TCP_MAX_SOCKS			(TCP_MAX_CONNS + USHRT_MAX * 2)
+
 struct ctx;
 
-void tcp_sock_handler(struct ctx *c, int s, uint32_t events, char *pkt_buf,
+void tcp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 		      struct timespec *now);
 int tcp_tap_handler(struct ctx *c, int af, void *addr,
 		    struct tap_msg *msg, int count, struct timespec *now);
@@ -13,24 +16,40 @@ int tcp_sock_init(struct ctx *c);
 void tcp_timer(struct ctx *c, struct timespec *ts);
 
 /**
+ * union tcp_epoll_ref - epoll reference portion for TCP connections
+ * @listen:		Set if this file descriptor is a listening socket
+ * @splice:		Set if descriptor is associated to a spliced connection
+ * @v6:			Set for IPv6 sockets or connections
+ * @index:		Index of connection in table, or port for bound sockets
+ * @u32:		Opaque u32 value of reference
+ */
+union tcp_epoll_ref {
+	struct {
+		uint32_t	listen:1,
+				splice:1,
+				v6:1,
+				index:20;
+	};
+	uint32_t u32;
+};
+
+/**
  * struct tcp_ctx - Execution context for TCP routines
  * @hash_secret:	128-bit secret for hash functions, ISN and hash table
- * @fd_min:		Lowest file descriptor number for TCP ever used
- * @fd_max:		Highest file descriptor number for TCP ever used
- * @fd_listen_min:	Lowest file descriptor number for listening sockets
- * @fd_listen_max:	Highest file descriptor number for listening sockets
- * @fd_conn_min:	Lowest file descriptor number for connected sockets
- * @fd_conn_max:	Highest file descriptor number for connected sockets
+ * @tap_conn_count:	Count of tap connections in connection table
+ * @splice_conn_count:	Count of spliced connections in connection table
+ * @port_to_tap:	Ports bound host/init-side, packets to guest/tap
+ * @port_to_init:	Ports bound namespace-side, spliced to init
+ * @port_to_ns:		Ports bound init-side, spliced to namespace
  * @timer_run:		Timestamp of most recent timer run
  */
 struct tcp_ctx {
 	uint64_t hash_secret[2];
-	int fd_min;
-	int fd_max;
-	int fd_listen_min;
-	int fd_listen_max;
-	int fd_conn_min;
-	int fd_conn_max;
+	int tap_conn_count;
+	int splice_conn_count;
+	uint8_t port_to_tap	[USHRT_MAX / 8];
+	uint8_t port_to_init	[USHRT_MAX / 8];
+	uint8_t port_to_ns	[USHRT_MAX / 8];
 	struct timespec timer_run;
 };
 

Makefile
@@ -1,13 +1,20 @@
 CFLAGS += -Wall -Wextra -pedantic
+CFLAGS += -DRLIMIT_STACK_VAL=$(shell ulimit -s)
 
-all: passt qrap
+all: passt pasta passt4netns qrap
 
 passt: passt.c passt.h arp.c arp.h dhcp.c dhcp.h dhcpv6.c dhcpv6.h pcap.c pcap.h  ndp.c ndp.h siphash.c siphash.h tap.c tap.h icmp.c icmp.h tcp.c tcp.h udp.c udp.h util.c util.h
 	$(CC) $(CFLAGS) passt.c arp.c dhcp.c dhcpv6.c pcap.c ndp.c siphash.c tap.c icmp.c tcp.c udp.c util.c -o passt
+
+pasta: passt
+	ln -s passt pasta
+
+passt4netns: passt
+	ln -s passt passt4netns
 
 qrap: qrap.c passt.h
 	$(CC) $(CFLAGS) -DARCH=\"$(shell uname -m)\" qrap.c -o qrap
 
 .PHONY: clean
 clean:
-	-${RM} passt *.o qrap
+	-${RM} passt *.o qrap pasta passt4netns

arp.c
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* PASST - Plug A Simple Socket Transport
+ *  for qemu/UNIX domain socket mode
+ *
+ * PASTA - Pack A Subtle Tap Abstraction
+ *  for network namespace/tap device mode
  *
  * arp.c - ARP implementation
  *
  * Copyright (c) 2020-2021 Red Hat GmbH
  * Author: Stefano Brivio <sbrivio@redhat.com>
- *
  */
 
 #include <stdio.h>
@@ -22,9 +25,9 @@
 #include <net/if_arp.h>
 #include <arpa/inet.h>
 
+#include "util.h"
 #include "passt.h"
 #include "dhcp.h"
-#include "util.h"
 #include "tap.h"
 #include "arp.h"
 
@@ -66,7 +69,7 @@ int arp(struct ctx *c, struct ethhdr *eh, size_t len)
 	memcpy(eh->h_dest, eh->h_source, ETH_ALEN);
 	memcpy(eh->h_source, c->mac, ETH_ALEN);
 
-	if (tap_send(c->fd_unix, eh, len, 0) < 0)
+	if (tap_send(c, eh, len, 0) < 0)
 		perror("ARP: send");
 
 	return 1;

dhcp.c
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* PASST - Plug A Simple Socket Transport
+ *  for qemu/UNIX domain socket mode
+ *
+ * PASTA - Pack A Subtle Tap Abstraction
+ *  for network namespace/tap device mode
  *
  * dhcp.c - Minimalistic DHCP server for PASST
  *
  * Copyright (c) 2020-2021 Red Hat GmbH
  * Author: Stefano Brivio <sbrivio@redhat.com>
- *
  */
 
 #include <stdio.h>
@@ -21,9 +24,9 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 
+#include "util.h"
 #include "passt.h"
 #include "dhcp.h"
-#include "util.h"
 #include "tap.h"
 
 /**
@@ -322,7 +325,7 @@ int dhcp(struct ctx *c, struct ethhdr *eh, size_t len)
 	memcpy(eh->h_dest, eh->h_source, ETH_ALEN);
 	memcpy(eh->h_source, c->mac, ETH_ALEN);
 
-	if (tap_send(c->fd_unix, eh, len, 0) < 0)
+	if (tap_send(c, eh, len, 0) < 0)
 		perror("DHCP: send");
 
 	return 1;

dhcpv6.c
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* PASST - Plug A Simple Socket Transport
+ *  for qemu/UNIX domain socket mode
+ *
+ * PASTA - Pack A Subtle Tap Abstraction
+ *  for network namespace/tap device mode
  *
  * dhcpv6.c - Minimalistic DHCPv6 server for PASST
  *
  * Copyright (c) 2021 Red Hat GmbH
  * Author: Stefano Brivio <sbrivio@redhat.com>
- *
  */
 
 #include <stdio.h>
@@ -23,9 +26,9 @@
 #include <net/if.h>
 #include <net/if_arp.h>
 
+#include "util.h"
 #include "passt.h"
 #include "tap.h"
-#include "util.h"
 
 /**
  * struct opt_hdr - DHCPv6 option header

icmp.c
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /* PASST - Plug A Simple Socket Transport
+ *  for qemu/UNIX domain socket mode
+ *
+ * PASTA - Pack A Subtle Tap Abstraction
+ *  for network namespace/tap device mode
  *
  * icmp.c - ICMP/ICMPv6 echo proxy
  *
  * Copyright (c) 2021 Red Hat GmbH
  * Author: Stefano Brivio <sbrivio@redhat.com>
- *
  */
 
 #include <stdio.h>
@@ -28,57 +31,91 @@
 #include <linux/icmpv6.h>
 #include <time.h>
 
+#include "util.h"
 #include "passt.h"
 #include "tap.h"
-#include "util.h"
 #include "icmp.h"
 
+#define ICMP_ECHO_TIMEOUT	60 /* s, timeout for ICMP socket activity */
+
+/**
+ * struct icmp_id - Tracking information for single ICMP echo identifier
+ * @sock:	Bound socket for identifier
+ * @ts:		Last associated activity from tap, seconds
+ * @seq:	Last sequence number sent to tap, host order
+ */
+struct icmp_id {
+	int sock;
+	time_t ts;
+	uint16_t seq;
+};
+
 /* Indexed by ICMP echo identifier */
-static int icmp_s_v4[USHRT_MAX];
-static int icmp_s_v6[USHRT_MAX];
+static struct icmp_id icmp_id_map	[IP_VERSIONS][USHRT_MAX];
+
+/* Bitmaps, activity monitoring needed for identifier */
+static uint8_t icmp_act			[IP_VERSIONS][USHRT_MAX / 8];
 
 /**
  * icmp_sock_handler() - Handle new data from socket
  * @c:		Execution context
- * @s:		File descriptor number for socket
+ * @ref:	epoll reference
  * @events:	epoll events bitmap
- * @pkt_buf:	Buffer to receive packets, currently unused
  * @now:	Current timestamp, unused
  */
-void icmp_sock_handler(struct ctx *c, int s, uint32_t events, char *pkt_buf,
+void icmp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 		       struct timespec *now)
 {
 	struct in6_addr a6 = { .s6_addr = {    0,    0,    0,    0,
 					       0,    0,    0,    0,
 					       0,    0, 0xff, 0xff,
 					       0,    0,    0,    0 } };
-	struct sockaddr_storage sr, sl;
-	socklen_t slen = sizeof(sr);
+	struct sockaddr_storage sr;
+	socklen_t sl = sizeof(sr);
 	char buf[USHRT_MAX];
+	uint16_t seq, id;
 	ssize_t n;
 
 	(void)events;
-	(void)pkt_buf;
 	(void)now;
 
-	n = recvfrom(s, buf, sizeof(buf), MSG_DONTWAIT,
-		     (struct sockaddr *)&sr, &slen);
+	n = recvfrom(ref.s, buf, sizeof(buf), 0, (struct sockaddr *)&sr, &sl);
 	if (n < 0)
 		return;
 
-	if (getsockname(s, (struct sockaddr *)&sl, &slen))
-		return;
+	if (ref.icmp.v6) {
+		struct sockaddr_in6 *sr6 = (struct sockaddr_in6 *)&sr;
+		struct icmp6hdr *ih = (struct icmp6hdr *)buf;
 
-	if (sl.ss_family == AF_INET) {
+		/* In PASTA mode, we'll get any reply we send, discard them. */
+		if (c->mode == MODE_PASTA) {
+			seq = ntohs(ih->icmp6_sequence);
+			id = ntohs(ih->icmp6_identifier);
+
+			if (icmp_id_map[V6][id].seq == seq)
+				return;
+
+			icmp_id_map[V6][id].seq = seq;
+		}
+
+		tap_ip_send(c, &sr6->sin6_addr, IPPROTO_ICMPV6, buf, n);
+	} else {
 		struct sockaddr_in *sr4 = (struct sockaddr_in *)&sr;
+		struct icmphdr *ih = (struct icmphdr *)buf;
+
+		if (c->mode == MODE_PASTA) {
+			seq = ntohs(ih->un.echo.sequence);
+			id = ntohs(ih->un.echo.id);
+
+			if (icmp_id_map[V4][id].seq == seq)
+				return;
+
+			icmp_id_map[V4][id].seq = seq;
+		}
 
 		memcpy(&a6.s6_addr[12], &sr4->sin_addr, sizeof(sr4->sin_addr));
 
 		tap_ip_send(c, &a6, IPPROTO_ICMP, buf, n);
-	} else if (sl.ss_family == AF_INET6) {
-		struct sockaddr_in6 *sr6 = (struct sockaddr_in6 *)&sr;
-
-		tap_ip_send(c, &sr6->sin6_addr, IPPROTO_ICMPV6, buf, n);
 	}
 }
 
@@ -86,101 +123,131 @@ void icmp_sock_handler(struct ctx *c, int s, uint32_t events, char *pkt_buf,
  * icmp_tap_handler() - Handle packets from tap
  * @c:		Execution context
  * @af:		Address family, AF_INET or AF_INET6
+ * @
  * @msg:	Input message
  * @count:	Message count (always 1 for ICMP)
- * @now:	Current timestamp, unused
+ * @now:	Current timestamp
  *
  * Return: count of consumed packets (always 1, even if malformed)
  */
 int icmp_tap_handler(struct ctx *c, int af, void *addr,
 		     struct tap_msg *msg, int count, struct timespec *now)
 {
-	int s;
-
 	(void)count;
-	(void)now;
-	(void)c;
 
 	if (af == AF_INET) {
 		struct icmphdr *ih = (struct icmphdr *)msg[0].l4h;
+		union icmp_epoll_ref iref = { .v6 = 0 };
 		struct sockaddr_in sa = {
 			.sin_family = AF_INET,
 			.sin_addr = { .s_addr = INADDR_ANY },
 			.sin_port = ih->un.echo.id,
 		};
+		int id, s;
 
 		if (msg[0].l4_len < sizeof(*ih) || ih->type != ICMP_ECHO)
 			return 1;
 
-		if ((s = icmp_s_v4[ntohs(ih->un.echo.id)]) < 0)
-			return 1;
+		id = ntohs(ih->un.echo.id);
 
-		bind(s, (struct sockaddr *)&sa, sizeof(sa));
+		if ((s = icmp_id_map[V4][id].sock) <= 0) {
+			s = sock_l4(c, AF_INET, IPPROTO_ICMP, id, 0, iref.u32);
+			if (s < 0)
+				goto fail_sock;
+
+			icmp_id_map[V4][id].sock = s;
+		}
+		icmp_id_map[V4][id].ts = now->tv_sec;
+		bitmap_set(icmp_act[V4], id);
 
 		sa.sin_addr = *(struct in_addr *)addr;
-		sendto(s, msg[0].l4h, msg[0].l4_len,
-		       MSG_DONTWAIT | MSG_NOSIGNAL,
+		sendto(s, msg[0].l4h, msg[0].l4_len, MSG_NOSIGNAL,
 		       (struct sockaddr *)&sa, sizeof(sa));
 	} else if (af == AF_INET6) {
 		struct icmp6hdr *ih = (struct icmp6hdr *)msg[0].l4h;
+		union icmp_epoll_ref iref = { .v6 = 1 };
 		struct sockaddr_in6 sa = {
 			.sin6_family = AF_INET6,
 			.sin6_addr = IN6ADDR_ANY_INIT,
 			.sin6_port = ih->icmp6_identifier,
 		};
+		int id, s;
 
 		if (msg[0].l4_len < sizeof(*ih) ||
 		    (ih->icmp6_type != 128 && ih->icmp6_type != 129))
 			return 1;
 
-		if ((s = icmp_s_v6[ntohs(ih->icmp6_identifier)]) < 0)
-			return 1;
+		id = ntohs(ih->icmp6_identifier);
+		if ((s = icmp_id_map[V6][id].sock) <= 0) {
+			s = sock_l4(c, AF_INET6, IPPROTO_ICMPV6, id, 0,
+				    iref.u32);
+			if (s < 0)
+				goto fail_sock;
 
-		bind(s, (struct sockaddr *)&sa, sizeof(sa));
+			icmp_id_map[V6][id].sock = s;
+		}
+		icmp_id_map[V6][id].ts = now->tv_sec;
+		bitmap_set(icmp_act[V6], id);
 
 		sa.sin6_addr = *(struct in6_addr *)addr;
-		sendto(s, msg[0].l4h, msg[0].l4_len,
-		       MSG_DONTWAIT | MSG_NOSIGNAL,
+		sendto(s, msg[0].l4h, msg[0].l4_len, MSG_NOSIGNAL,
 		       (struct sockaddr *)&sa, sizeof(sa));
 	}
 
 	return 1;
+
+fail_sock:
+	warn("Cannot open \"ping\" socket. You might need to:");
+	warn("  sysctl -w net.ipv4.ping_group_range=\"0 2147483647\"");
+	warn("...echo requests/replies will fail.");
+	return 1;
 }
 
 /**
- * icmp_sock_init() - Create ICMP, ICMPv6 sockets for echo requests and replies
+ * icmp_timer_one() - Handler for timed events related to a given identifier
  * @c:		Execution context
- *
- * Return: 0 on success, -1 on failure
+ * @v6:		Set for IPv6 echo identifier bindings
+ * @id:		Echo identifier, host order
+ * @ts:		Timestamp from caller
  */
-int icmp_sock_init(struct ctx *c)
+static void icmp_timer_one(struct ctx *c, int v6, uint16_t id,
+			   struct timespec *ts)
 {
-	int i, fail = 0;
+	struct icmp_id *id_map = &icmp_id_map[v6 ? V6 : V4][id];
 
-	c->icmp.fd_min = INT_MAX;
-	c->icmp.fd_max = 0;
+	if (ts->tv_sec - id_map->ts <= ICMP_ECHO_TIMEOUT)
+		return;
 
-	if (c->v4) {
-		for (i = 0; i < USHRT_MAX; i++) {
-			icmp_s_v4[i] = sock_l4(c, AF_INET, IPPROTO_ICMP, i);
-			if (icmp_s_v4[i] < 0)
-				fail = 1;
+	bitmap_clear(icmp_act[v6 ? V6 : V4], id);
+
+	epoll_ctl(c->epollfd, EPOLL_CTL_DEL, id_map->sock, NULL);
+	close(id_map->sock);
+	id_map->sock = 0;
+}
+
+/**
+ * icmp_timer() - Scan activity bitmap for identifiers with timed events
+ * @c:		Execution context
+ * @ts:		Timestamp from caller
+ */
+void icmp_timer(struct ctx *c, struct timespec *ts)
+{
+	long *word, tmp;
+	unsigned int i;
+	int n, v6 = 0;
+
+v6:
+	word = (long *)icmp_act[v6 ? V6 : V4];
+	for (i = 0; i < sizeof(icmp_act[0]) / sizeof(long); i++, word++) {
+		tmp = *word;
+		while ((n = ffsl(tmp))) {
+			tmp &= ~(1UL << (n - 1));
+			icmp_timer_one(c, v6, i * sizeof(long) * 8 + n - 1, ts);
 		}
 	}
 
-	if (c->v6) {
-		for (i = 0; i < USHRT_MAX; i++) {
-			icmp_s_v6[i] = sock_l4(c, AF_INET6, IPPROTO_ICMPV6, i);
-			if (icmp_s_v6[i] < 0)
-				fail = 1;
-		}
+	if (!v6) {
+		v6 = 1;
+		goto v6;
 	}
-
-	if (fail) {
-		warn("Cannot open some \"ping\" sockets. You might need to:");
-		warn("  sysctl -w net.ipv4.ping_group_range=\"0 2147483647\"");
-		warn("...echo requests/replies might fail.");
-	}
-
-	return 0;
 }

icmp.h
@@ -1,22 +1,34 @@
 #ifndef ICMP_H
 #define ICMP_H
 
+#define ICMP_TIMER_INTERVAL		1000 /* ms */
+
 struct ctx;
 
-void icmp_sock_handler(struct ctx *c, int s, uint32_t events, char *pkt_buf,
+void icmp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 		       struct timespec *now);
 int icmp_tap_handler(struct ctx *c, int af, void *addr,
 		     struct tap_msg *msg, int count, struct timespec *now);
-int icmp_sock_init(struct ctx *c);
+void icmp_timer(struct ctx *c, struct timespec *ts);
+
+/**
+ * union icmp_epoll_ref - epoll reference portion for ICMP tracking
+ * @v6:			Set for IPv6 sockets or connections
+ * @u32:		Opaque u32 value of reference
+ */
+union icmp_epoll_ref {
+	struct {
+		uint32_t	v6:1;
+	};
+	uint32_t u32;
+};
 
 /**
  * struct icmp_ctx - Execution context for ICMP routines
- * @fd_min:	Lowest file descriptor number for ICMP/ICMPv6 ever used
- * @fd_max:	Highest file descriptor number for ICMP/ICMPv6 ever used
+ * @timer_run:		Timestamp of most recent timer run
  */
 struct icmp_ctx {
-	int fd_min;
-	int fd_max;
+	struct timespec timer_run;
 };
 
 #endif /* ICMP_H */

commit d0dd0242a69f4ae5be4f46030bf321eb6867b2f8

tcp, tcp_splice: Fix port remapping for inbound, spliced connections
In pasta mode, when we receive a new inbound connection, we need to
select a socket that was created in the namespace to proceed and
connect() it to its final destination.

The existing condition might pick a wrong socket, though, if the
destination port is remapped, because we'll check the bitmap of
inbound ports using the remapped port (stored in the epoll reference)
as index, and not the original port.

Instead of using the port bitmap for this purpose, store this
information in the epoll reference itself, by adding a new 'outbound'
bit, that's set if the listening socket was created the namespace,
and unset otherwise.

Then, use this bit to pick a socket on the right side.

Suggested-by: David Gibson <david@gibson.dropbear.id.au>
Fixes: 33482d5bf293 ("passt: Add PASTA mode, major rework")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

tcp.c
@@ -3084,15 +3084,14 @@ void tcp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 void tcp_sock_init(const struct ctx *c, int ns, sa_family_t af,
 		   const void *addr, const char *ifname, in_port_t port)
 {
-	union tcp_epoll_ref tref = { .tcp.listen = 1 };
+	union tcp_epoll_ref tref = { .tcp.listen = 1, .tcp.outbound = ns };
 	const void *bind_addr;
 	int s;
 
-	if (ns) {
+	if (ns)
 		tref.tcp.index = (in_port_t)(port + c->tcp.fwd_out.delta[port]);
-	} else {
+	else
 		tref.tcp.index = (in_port_t)(port + c->tcp.fwd_in.delta[port]);
-	}
 
 	if (af == AF_INET || af == AF_UNSPEC) {
 		if (!addr && c->mode == MODE_PASTA)

tcp.h
@@ -34,6 +34,7 @@ void tcp_update_l2_buf(const unsigned char *eth_d, const unsigned char *eth_s,
  * union tcp_epoll_ref - epoll reference portion for TCP connections
  * @listen:		Set if this file descriptor is a listening socket
  * @splice:		Set if descriptor is associated to a spliced connection
+ * @outbound:		Listening socket maps to outbound, spliced connection
  * @v6:			Set for IPv6 sockets or connections
  * @timer:		Reference is a timerfd descriptor for connection
  * @index:		Index of connection in table, or port for bound sockets
@@ -43,6 +44,7 @@ union tcp_epoll_ref {
 	struct {
 		uint32_t	listen:1,
 				splice:1,
+				outbound:1,
 				v6:1,
 				timer:1,
 				index:20;

tcp_splice.c
@@ -35,6 +35,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
@@ -506,19 +507,19 @@ static int tcp_splice_connect_ns(void *arg)
  * @c:		Execution context
  * @conn:	Connection pointer
  * @port:	Destination port, host order
+ * @outbound:	Connection request coming from namespace
  *
  * Return: return code from connect()
  */
 static int tcp_splice_new(const struct ctx *c, struct tcp_splice_conn *conn,
-			  in_port_t port)
+			  in_port_t port, int outbound)
 {
-	struct tcp_splice_connect_ns_arg ns_arg = { c, conn, port, 0 };
 	int *p, i, s = -1;
 
-	if (bitmap_isset(c->tcp.fwd_in.map, port))
-		p = CONN_V6(conn) ? ns_sock_pool6 : ns_sock_pool4;
-	else
+	if (outbound)
 		p = CONN_V6(conn) ? init_sock_pool6 : init_sock_pool4;
+	else
+		p = CONN_V6(conn) ? ns_sock_pool6 : ns_sock_pool4;
 
 	for (i = 0; i < TCP_SOCK_POOL_SIZE; i++, p++) {
 		SWAP(s, *p);
@@ -526,11 +527,15 @@ static int tcp_splice_new(const struct ctx *c, struct tcp_splice_conn *conn,
 			break;
 	}
 
-	if (s < 0 && bitmap_isset(c->tcp.fwd_in.map, port)) {
+	/* No socket available in namespace: create a new one for connect() */
+	if (s < 0 && !outbound) {
+		struct tcp_splice_connect_ns_arg ns_arg = { c, conn, port, 0 };
+
 		NS_CALL(tcp_splice_connect_ns, &ns_arg);
 		return ns_arg.ret;
 	}
 
+	/* Otherwise, the socket will connect on the side it was created on */
 	return tcp_splice_connect(c, conn, s, port);
 }
 
@@ -592,7 +597,8 @@ void tcp_sock_handler_splice(struct ctx *c, union epoll_ref ref,
 		conn->a = s;
 		conn->flags = ref.r.p.tcp.tcp.v6 ? SOCK_V6 : 0;
 
-		if (tcp_splice_new(c, conn, ref.r.p.tcp.tcp.index))
+		if (tcp_splice_new(c, conn, ref.r.p.tcp.tcp.index,
+				   ref.r.p.tcp.tcp.outbound))
 			conn_flag(c, conn, CLOSING);
 
 		return;