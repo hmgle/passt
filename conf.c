hmgle/passt

commit 3d0de2c1d72757a7754532f788b53102420e987d

conf, tcp, udp: Exit if we fail to bind sockets for all given ports
passt supports ranges of forwarded ports as well as 'all' for TCP and
UDP, so it might be convenient to proceed if we fail to bind only
some of the desired ports.

But if we fail to bind even a single port for a given specification,
we're clearly, unexpectedly, conflicting with another network
service. In that case, report failure and exit.

Reported-by: Yalan Zhang <yalzhang@redhat.com>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

conf.c
@@ -179,9 +179,9 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 {
 	char addr_buf[sizeof(struct in6_addr)] = { 0 }, *addr = addr_buf;
 	char buf[BUFSIZ], *spec, *ifname = NULL, *p;
+	bool exclude_only = true, bound_one = false;
 	uint8_t exclude[PORT_BITMAP_SIZE] = { 0 };
 	sa_family_t af = AF_UNSPEC;
-	bool exclude_only = true;
 
 	if (!strcmp(optarg, "none")) {
 		if (fwd->mode)
@@ -215,11 +215,18 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 		memset(fwd->map, 0xff, PORT_EPHEMERAL_MIN / 8);
 
 		for (i = 0; i < PORT_EPHEMERAL_MIN; i++) {
-			if (optname == 't')
-				tcp_sock_init(c, AF_UNSPEC, NULL, NULL, i);
-			else if (optname == 'u')
-				udp_sock_init(c, 0, AF_UNSPEC, NULL, NULL, i);
+			if (optname == 't') {
+				if (!tcp_sock_init(c, AF_UNSPEC, NULL, NULL, i))
+					bound_one = true;
+			} else if (optname == 'u') {
+				if (!udp_sock_init(c, 0, AF_UNSPEC, NULL, NULL,
+						   i))
+					bound_one = true;
+			}
 		}
+
+		if (!bound_one)
+			goto bind_fail;
 
 		return;
 	}
@@ -293,11 +300,17 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 
 			bitmap_set(fwd->map, i);
 
-			if (optname == 't')
-				tcp_sock_init(c, af, addr, ifname, i);
-			else if (optname == 'u')
-				udp_sock_init(c, 0, af, addr, ifname, i);
+			if (optname == 't') {
+				if (!tcp_sock_init(c, af, addr, ifname, i))
+					bound_one = true;
+			} else if (optname == 'u') {
+				if (!udp_sock_init(c, 0, af, addr, ifname, i))
+					bound_one = true;
+			}
 		}
+
+		if (!bound_one)
+			goto bind_fail;
 
 		return;
 	}
@@ -339,12 +352,18 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 
 			fwd->delta[i] = mapped_range.first - orig_range.first;
 
-			if (optname == 't')
-				tcp_sock_init(c, af, addr, ifname, i);
-			else if (optname == 'u')
-				udp_sock_init(c, 0, af, addr, ifname, i);
+			if (optname == 't') {
+				if (!tcp_sock_init(c, af, addr, ifname, i))
+					bound_one = true;
+			} else if (optname == 'u') {
+				if (udp_sock_init(c, 0, af, addr, ifname, i))
+					bound_one = true;
+			}
 		}
 	} while ((p = next_chunk(p, ',')));
+
+	if (!bound_one)
+		goto bind_fail;
 
 	return;
 bad:
@@ -353,6 +372,8 @@ overlap:
 	die("Overlapping port specifier %s", optarg);
 mode_conflict:
 	die("Port forwarding mode '%s' conflicts with previous mode", optarg);
+bind_fail:
+	die("Failed to bind any port for '-%c %s', exiting", optname, optarg);
 }
 
 /**

tcp.c
@@ -2916,20 +2916,31 @@ static int tcp_sock_init_af(const struct ctx *c, int af, in_port_t port,
  * @addr:	Pointer to address for binding, NULL if not configured
  * @ifname:	Name of interface to bind to, NULL if not configured
  * @port:	Port, host order
+ *
+ * Return: 0 on (partial) success, -1 on (complete) failure
  */
-void tcp_sock_init(const struct ctx *c, sa_family_t af, const void *addr,
-		   const char *ifname, in_port_t port)
+int tcp_sock_init(const struct ctx *c, sa_family_t af, const void *addr,
+		  const char *ifname, in_port_t port)
 {
+	int ret = 0;
+
 	if (af == AF_UNSPEC && c->ifi4 && c->ifi6)
 		/* Attempt to get a dual stack socket */
 		if (tcp_sock_init_af(c, AF_UNSPEC, port, addr, ifname) >= 0)
-			return;
+			return 0;
 
 	/* Otherwise create a socket per IP version */
-	if ((af == AF_INET  || af == AF_UNSPEC) && c->ifi4)
-		tcp_sock_init_af(c, AF_INET, port, addr, ifname);
-	if ((af == AF_INET6 || af == AF_UNSPEC) && c->ifi6)
-		tcp_sock_init_af(c, AF_INET6, port, addr, ifname);
+	if ((af == AF_INET  || af == AF_UNSPEC) && c->ifi4) {
+		if (tcp_sock_init_af(c, AF_INET, port, addr, ifname) < 0)
+			ret = -1;
+	}
+
+	if ((af == AF_INET6 || af == AF_UNSPEC) && c->ifi6) {
+		if (tcp_sock_init_af(c, AF_INET6, port, addr, ifname) < 0)
+			ret = -1;
+	}
+
+	return ret;
 }
 
 /**

tcp.h
@@ -17,8 +17,8 @@ void tcp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 		      const struct timespec *now);
 int tcp_tap_handler(struct ctx *c, int af, const void *addr,
 		    const struct pool *p, const struct timespec *now);
-void tcp_sock_init(const struct ctx *c, sa_family_t af, const void *addr,
-		   const char *ifname, in_port_t port);
+int tcp_sock_init(const struct ctx *c, sa_family_t af, const void *addr,
+		  const char *ifname, in_port_t port);
 int tcp_init(struct ctx *c);
 void tcp_timer(struct ctx *c, const struct timespec *ts);
 void tcp_defer_handler(struct ctx *c);

udp.c
@@ -955,12 +955,14 @@ int udp_tap_handler(struct ctx *c, int af, const void *addr,
  * @addr:	Pointer to address for binding, NULL if not configured
  * @ifname:	Name of interface to bind to, NULL if not configured
  * @port:	Port, host order
+ *
+ * Return: 0 on (partial) success, -1 on (complete) failure
  */
-void udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
-		   const void *addr, const char *ifname, in_port_t port)
+int udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
+		  const void *addr, const char *ifname, in_port_t port)
 {
 	union udp_epoll_ref uref = { .u32 = 0 };
-	int s;
+	int s, ret = 0;
 
 	if (ns) {
 		uref.udp.port = (in_port_t)(port +
@@ -989,6 +991,9 @@ void udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
 				    ifname, port, uref.u32);
 			udp_splice_ns[V4][port].sock = s;
 		}
+
+		if (s < 0)
+			ret = -1;
 	}
 
 	if ((af == AF_INET6 || af == AF_UNSPEC) && c->ifi6) {
@@ -1009,7 +1014,12 @@ void udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
 				    ifname, port, uref.u32);
 			udp_splice_ns[V6][port].sock = s;
 		}
+
+		if (s < 0)
+			ret = -1;
 	}
+
+	return ret;
 }
 
 /**

udp.h
@@ -12,8 +12,8 @@ void udp_sock_handler(struct ctx *c, union epoll_ref ref, uint32_t events,
 		      const struct timespec *now);
 int udp_tap_handler(struct ctx *c, int af, const void *addr,
 		    const struct pool *p, const struct timespec *now);
-void udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
-		   const void *addr, const char *ifname, in_port_t port);
+int udp_sock_init(const struct ctx *c, int ns, sa_family_t af,
+		  const void *addr, const char *ifname, in_port_t port);
 int udp_init(struct ctx *c);
 void udp_timer(struct ctx *c, const struct timespec *ts);
 void udp_update_l2_buf(const unsigned char *eth_d, const unsigned char *eth_s,

commit 4663ccc89a7fcbf9d901a80730ee925fc7f64c59

conf: Fix typo and logic in conf_ports() check for port binding
Ouch, I accidentally pushed the previous change without running the
tests:

- we need to check, in conf_ports(), that udp_sock_init()
  managed to bind at least a port, not the opposite

- for -T and -U, we have no way to know if we'll manage to bind
  the port later, so never report an error for those

Fixes: 3d0de2c1d727 ("conf, tcp, udp: Exit if we fail to bind sockets for all given ports")
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

conf.c
@@ -306,6 +306,9 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 			} else if (optname == 'u') {
 				if (!udp_sock_init(c, 0, af, addr, ifname, i))
 					bound_one = true;
+			} else {
+				/* No way to check in advance for -T and -U */
+				bound_one = true;
 			}
 		}
 
@@ -356,8 +359,11 @@ static void conf_ports(const struct ctx *c, char optname, const char *optarg,
 				if (!tcp_sock_init(c, af, addr, ifname, i))
 					bound_one = true;
 			} else if (optname == 'u') {
-				if (udp_sock_init(c, 0, af, addr, ifname, i))
+				if (!udp_sock_init(c, 0, af, addr, ifname, i))
 					bound_one = true;
+			} else {
+				/* No way to check in advance for -T and -U */
+				bound_one = true;
 			}
 		}
 	} while ((p = next_chunk(p, ',')));