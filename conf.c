hmgle/passt

commit 89e38f55405d00ca00ec64936531d8db18954a95

treewide: Fix header includes to build with musl
Roughly inspired from a patch by Chris Kuhn: fix up includes so that
we can build against musl: glibc is more lenient as headers generally
include a larger amount of other headers.

Compared to the original patch, I only included what was needed
directly in C files, instead of adding blanket includes in local
header files. It's a bit more involved, but more consistent with the
current (not ideal) situation.

Reported-by: Chris Kuhn <kuhnchris+github@kuhnchris.eu>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

conf.c
@@ -23,8 +23,10 @@
 #include <limits.h>
 #include <grp.h>
 #include <pwd.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <syslog.h>

netlink.c
@@ -18,6 +18,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <limits.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>

tap.c
@@ -14,6 +14,7 @@
  */
 
 #include <sched.h>
+#include <signal.h>
 #include <stdio.h>
 #include <errno.h>
 #include <limits.h>

tcp.c
@@ -267,6 +267,7 @@
 #include <sched.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <limits.h>

tcp_splice.c
@@ -32,6 +32,7 @@
  */
 
 #include <sched.h>
+#include <signal.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>

udp.c
@@ -91,6 +91,7 @@
  */
 
 #include <sched.h>
+#include <signal.h>
 #include <stdio.h>
 #include <errno.h>
 #include <limits.h>

isolation.c
@@ -65,6 +65,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>

passt.c
@@ -27,6 +27,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <time.h>

commit 34ade90957ddc5086cc17dc1af47ca640aa650e5

Work around weird false positives with cppcheck-2.9.1
Commit 89e38f55 "treewide: Fix header includes to build with musl" added
extra #includes to work with musl.  Unfortunately with the cppcheck version
I'm using (cppcheck-2.9-1.fc37.x86_64 in Fedora 37) this causes weird false
positives: specifically cppcheck seems to hit a #error in <bits/unistd.h>
complaining about including it directly instead of via <unistd.h> (which is
not something we're doing).

I have no idea why that would be happening; but I'm guessing it has to be
a bug in the cpp implementation in that cppcheck version.  In any case,
it's possible to work around this by moving the include of <unistd.h>
before the include of <signal.h>.  So, do that.

Fixes: 89e38f55405d ("treewide: Fix header includes to build with musl")
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

conf.c
@@ -23,12 +23,12 @@
 #include <limits.h>
 #include <grp.h>
 #include <pwd.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
 #include <syslog.h>
 #include <time.h>
 #include <netinet/in.h>

netlink.c
@@ -18,11 +18,11 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <limits.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>

tap.c
@@ -14,6 +14,7 @@
  */
 
 #include <sched.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 #include <errno.h>
@@ -33,7 +34,6 @@
 #include <sys/uio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>

tcp.c
@@ -267,6 +267,7 @@
 #include <sched.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -287,7 +288,6 @@
 #include <sys/timerfd.h>
 #include <sys/types.h>
 #include <sys/uio.h>
-#include <unistd.h>
 #include <time.h>
 
 #include <linux/tcp.h> /* For struct tcp_info */

tcp_splice.c
@@ -32,6 +32,7 @@
  */
 
 #include <sched.h>
+#include <unistd.h>
 #include <signal.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -40,7 +41,6 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
 #include <net/ethernet.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>

udp.c
@@ -91,6 +91,7 @@
  */
 
 #include <sched.h>
+#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 #include <errno.h>
@@ -107,7 +108,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
-#include <unistd.h>
 #include <time.h>
 
 #include "checksum.h"